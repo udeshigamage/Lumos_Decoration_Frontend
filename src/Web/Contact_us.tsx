1e14b771588455c3d63b5843c4e1a5ee2702ab5e
udeshi <udeshigamage92@gmail.com>
2024-12-30 02:00:21 +0530

    initial_commit

---
+++ src/Web/Contact_us.tsx
@@ -0,0 +1,60 @@
+import React from "react";
+import pic08 from "../assets/pic08.jpg";
+
+const Contact_us = () => {
+  return (
+    <div>
+      <div className="flex  flex-row min-h-screen ">
+        <div className="basis-1/3">
+          <img src={pic08} className="h-full object-cover object-right" />
+        </div>
+
+        <div className="basis-1/3">
+          <div className="flex flex-col">
+            <div className="px-5 py-5">
+              <h1 className="text-5xl">Contact Us</h1>
+            </div>
+            <div className="px-5 py-5">
+              <input
+                type="text"
+                placeholder="Full name"
+                className="input input-bordered w-full max-w-md"
+              />
+            </div>
+            <div className="px-5 py-5">
+              <input
+                type="email"
+                placeholder="Email"
+                className="input input-bordered w-full max-w-md "
+              />
+            </div>
+            <div className="px-5 py-5">
+              <textarea
+                placeholder="Message"
+                className="textarea textarea-bordered textarea-lg w-full max-w-md"
+              ></textarea>
+            </div>
+            <div className="px-5 py-5">
+              <button className="btn btn-wide">Contact us</button>
+            </div>
+          </div>
+        </div>
+        <div className="basis-1/3 ">
+          <div className="flex flex-row items-start justify-center min-h-screen">
+            <div className="px-5 py-5 self-center z-30">
+              <h1>Address</h1>
+              <span>no 262/E</span>
+              <span>ihalabiyanwiiila</span>
+              <span>mankada road</span>
+              <span> kadawtha</span>
+              <h1 className="mt-5">Contact Number</h1>
+              <span>077-1234567</span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact_us;
---
+++ src/layout.tsx
@@ -0,0 +1,11 @@
+import Contact_us from "./Web/Contact_us";
+
+function Layout() {
+  return (
+    <>
+      <Contact_us />
+    </>
+  );
+}
+
+export default Layout;
