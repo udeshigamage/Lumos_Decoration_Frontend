438201dee4fefd62d42db47d35b6975a6e7aeaeb
udeshi <udeshigamage92@gmail.com>
2025-01-01 10:00:00 +0530

    modified customer interface

---
+++ src/CustomerInterface/Myorders.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Myorders = () => {
+  return (
+    <div>
+      <h1>My orders</h1>
+      <div className="flex flex-rowposition-relative gap-5">
+        <div className="basis-1/3 my-20 mx-5"></div>
+        <div className="basis-1/3 my-20 mx-5"></div>
+        <div className="basis-1/3 my-20 mx-5"></div>
+      </div>
+    </div>
+  );
+};
+
+export default Myorders;
