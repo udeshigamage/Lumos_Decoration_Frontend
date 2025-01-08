d89684882737c947224e3a40a5800639e4f910dc
udeshi <udeshigamage92@gmail.com>
2025-01-08 10:00:00 +0530

    modified customer interface

--- src/CustomerInterface/Ordersummary.tsx
+++ src/CustomerInterface/Ordersummary.tsx
@@ -8,6 +8,26 @@ const Ordersummary = () => {
         <label className="label">Choose delivery date</label>
         <input type="date" className="input input-bordered w-full max-w-xs" />
       </div>
+      <div className="flex position-relative gap-5">
+        <input type="checkbox" className="checkbox" />
+        <label className="label">Flower boquet</label>
+        <input type="checkbox" className="checkbox" />
+        <label className="label">Flower boquet</label>
+      </div>
+      <div className="flex position-relative gap-5">
+        <textarea
+          className="textarea textarea-bordered h-24 w-96"
+          placeholder="Write your message here"
+        ></textarea>
+        <textarea
+          className="textarea textarea-bordered h-24 w-96"
+          placeholder="Write your message here"
+        ></textarea>
+      </div>
+      <div className="flex position-relative gap-5">
+        <button className="btn btn-primary">Submit</button>
+        <button className="btn btn-primary">Cancel</button>
+      </div>
     </div>
   );
 };
