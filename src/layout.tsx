b689ca96d514c0c0b8fb8c5d7be68ebb39259c9a
udeshi <udeshigamage92@gmail.com>
2025-01-16 10:00:00 +0530

    Added modification layout

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,15 +1,19 @@
 import About_us from "./Web/About_us";
 import Contact_us from "./Web/Contact_us";
+import Footer from "./Web/Footer";
 import Homepage from "./Web/Homepage";
 import Navbar from "./Web/Navbar";
+import Services from "./Web/Services";
 
 function Layout() {
   return (
     <>
       <Navbar />
       <Homepage />
+      <Services />
       <About_us />
       <Contact_us />
+      <Footer />
     </>
   );
 }
